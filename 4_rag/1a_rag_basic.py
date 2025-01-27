7ff4222e09fa648018bd7a8df06757f4136319d1
Arup43 <debnatharupbzs@gmail.com>
2025-01-27 20:23:31 +0600

    feat(RAG): retrieve relevant documents from vector db

--- 4_rag/1a_rag_basic.py
+++ 4_rag/1a_rag_basic.py
@@ -2,9 +2,8 @@ import os
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_google_genai import (
-    ChatGoogleGenerativeAI,
     GoogleGenerativeAIEmbeddings  # Updated name
 )
 
